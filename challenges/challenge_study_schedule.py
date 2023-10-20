816daf8785bc8d1b670fec43f6b46a059c02cb36
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-20 21:28:21 +0000

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    count = 0
+    for element in permanence_period:
+        start, end = element
+        if not isinstance(start, int) or not isinstance(end, int):
+            count = None
+            break
+        if target_time in range(element[0], element[1] + 1):
+            count += 1
+    return count
