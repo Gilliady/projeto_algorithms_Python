267708610c88b8bdc3cb887fab12d0965157cb35
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-20 22:29:44 +0000

    requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    word = word.upper()
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return word[high_index] == word[low_index]
+    if low_index >= high_index:
+        return True
+    return word[low_index] == word[high_index] and is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+        )
+
