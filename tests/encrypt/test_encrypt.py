5b8c83f4ccfb2398e08042e67b794d61f945e06c
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-20 21:57:50 +0000

    requisito 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("abcde", 1) == "a_edcb"
+    assert encrypt_message("abcde", 2) == "edc_ba"
+    assert encrypt_message("abc", 9) == "cba"
+    assert encrypt_message("accb", 2) == "bc_ca"
+    with pytest.raises(TypeError):
+        encrypt_message(123, 1)
