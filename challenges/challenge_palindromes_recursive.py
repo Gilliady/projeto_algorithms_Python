961416ea63d5febfb591ffc065beeab2c84d08b1
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-20 22:30:11 +0000

    warning

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,4 +9,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     return word[low_index] == word[high_index] and is_palindrome_recursive(
         word, low_index + 1, high_index - 1
         )
-
