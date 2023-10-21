9e4f027a2431e88de4f47fc46fa1b8d23d118b7a
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-21 01:02:39 +0000

    requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,28 @@
+def sort_string(string):
+    if len(string) < 2:
+        return string
+    low, same, high = [], [], []
+
+    pivot = string[len(string) // 2]
+    for item in string:
+        if item < pivot:
+            low.append(item)
+        elif item > pivot:
+            high.append(item)
+        else:
+            same.append(item)
+    return ''.join(sort_string(low)) + ''.join(
+        same
+        ) + ''.join(sort_string(high))
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = sort_string(first_string.lower())
+    second_string = sort_string(second_string.lower())
+    if not first_string or not second_string:
+        resposta = (first_string, second_string, False)
+        return resposta
+    if first_string == second_string:
+        resposta = (first_string, second_string, True)
+        return resposta
+    return (first_string, second_string, False)
