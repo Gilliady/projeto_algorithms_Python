da3a0e8f72675af3c4d64e3bf87110bac2b7add0
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-23 14:14:55 +0000

    requisito 5 seja o que Deus quiser

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,23 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def validate_input(nums):
+    if not nums or isinstance(nums[0], str) or len(nums) < 2:
+        return False
+    for num in nums:
+        if 0 >= num or not isinstance(num, int):
+            return False
+    return True
+
+
+def find_duplicate(nums: list):
+    if not validate_input(nums):
+        return False
+    nums = sorted(nums, key=lambda x: nums.count(x) > 1, reverse=True)
+    nums_b = []
+    for num in nums:
+        if num in nums_b:
+            return num
+        nums_b.append(num)
+    return False
+
+
+if __name__ == "__main__":
+    find_duplicate([3, 1, 3, 4, 2])
